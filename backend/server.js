fa09cb5a89e103092014f4c10e4efc32120477a0
Tristan Trouwen <tristan.trouwen@gmail.com>
2019-02-02 22:03:23 +0100

    added headers, so api is available for other domains

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,18 @@ let config = require(__dirname + '/config');
 
 server.listen(config.port);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 app.get('/data', function (req, res) {
     fs.readFile(__dirname + '/data.json', 'utf8', (err, data) => {
          res.end(data);
       });
 });
+
 
 console.log('[*] Info: Running on http://' + config.server + ':' + config.port.toString());
