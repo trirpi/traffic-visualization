6f2a3a25527a96291caf8d4ebe444bc608f17255
Tristan Trouwen <tristan.trouwen@gmail.com>
2019-02-02 16:00:11 +0100

    improved setup

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-};
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+        ]
+    }
+}
