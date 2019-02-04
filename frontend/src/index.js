493e75b97ac68d78c68711d1dd294a5f5a9e3a9e
Tristan Trouwen <tristan.trouwen@gmail.com>
2019-02-05 00:06:10 +0100

    created basis map

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,29 +1,8 @@
 import * as d3 from 'd3';
-import * as topojson from 'topojson-client';
+import * as L from 'leaflet';
 
-let geojson = {
-  "type": "Feature",
-  "geometry": {
-    "type": "Point",
-    "coordinates": [125.6, 10.1]
-  },
-  "properties": {
-    "name": "Dinagat Islands"
-  }
-};
-
-
-let width = 960;
-let height = 500;
-
-let projection = d3.geoMercator();
-
-let path = d3.geoPath()
-    .projection(projection)
-
-let svg = d3.select("#content").append("svg")
-   .attr("width", width)
-   .attr("height", height);
+let map = L.map('map');
+map.setView([51.505, -0.09], 13);
 
 let url = "http://localhost:3000/data"
 
@@ -32,13 +11,3 @@ d3.json(url).then(data => {
 });
 
 
-let url1 = "http://localhost:3000/belgium_data"
-d3.json(url1).then(topology => {
-    let geojson = topology;
-    console.log("geojson", geojson)
-
-    svg.selectAll("path")
-        .data(geojson.features)
-      .enter().append("path")
-        .attr("d", path);
-});
