41aca4e1b2fb2af3c7f001f670c302c86d906a99
Tristan Trouwen <tristan.trouwen@gmail.com>
2019-02-05 17:58:49 +0100

    added markers for measure points

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,25 @@ import * as d3 from 'd3';
 import * as L from 'leaflet';
 
 let map = L.map('map');
-map.setView([51.505, -0.09], 13);
 
-let url = "http://localhost:3000/data"
+let osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+let osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a>';
+
+let osm = new L.TileLayer(osmUrl, {minZoom: 6, maxZoom: 10, attribution: osmAttrib});		
+
+map.setView(new L.LatLng(51.0, 4.45),8);
+map.addLayer(osm);
+
+let url = 'http://localhost:3000/data';
 
 d3.json(url).then(data => {
-    console.log("data", data);
+    for (var key in data) {
+        var circle = L.circle([data[key]['latitude'], data[key]['longitude']], {
+            color: 'red',
+            fillColor: '#f03',
+            radius: 10
+        }).addTo(map);
+    }
 });
 
 
