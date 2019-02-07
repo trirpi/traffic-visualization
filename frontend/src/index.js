7f4b57abfa33f4c67b3b5256d2443a8bd43c4253
Tristan Trouwen <tristan.trouwen@gmail.com>
2019-02-07 11:50:17 +0100

    made continuous updating work fluently

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,3 @@
-import * as d3 from 'd3';
 import * as L from 'leaflet';
 
 let map = L.map('map');
@@ -8,50 +7,53 @@ let osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a>
 
 let osm = new L.TileLayer(osmUrl, {minZoom: 6, maxZoom: 15, attribution: osmAttrib});
 
-map.setView(new L.LatLng(51.0, 4.45),8);
+map.setView(new L.LatLng(51.0, 4.45),9);
 map.addLayer(osm);
 
 let url = 'http://localhost:3000/data';
 
 let markers = [];
+let old_markers = [];
 
 function updateMarkers() {
-    for (var marker in markers) {
-        map.removeLayer(marker);
-        console.log(marker);
-    }
+    old_markers = markers;
     markers = [];
-    d3.json(url).then(data => {
-
-        for (var key in data) {
-            let speed = data[key]['speed'];
-            let r = 0;
-            let g = 0;
-            let b = 255;
-            if (speed > 0 && speed <= 250) {
-                if (speed > 85) {
-                    r = 0;
-                    g = 255;
-                    b = 0;
-                } else {
-                    r = 255;
-                    g = speed*2;
-                    b = 0;
+    fetch(url)
+        .then(response => {
+            return response.json();
+        }).then(data => {
+            for (var key in data) {
+                let speed = data[key]['speed'];
+                let r = 0;
+                let g = 0;
+                let b = 255;
+                if (speed > 0 && speed <= 250) {
+                    if (speed > 85) {
+                        r = 0;
+                        g = 255;
+                        b = 0;
+                    } else {
+                        r = 255;
+                        g = speed*2;
+                        b = 0;
+                    }
                 }
+
+                let circle = L.circle([data[key]['latitude'], data[key]['longitude']], {
+                    color: 'rgb('+r/2+','+g/2+', '+b/2+')',
+                    fillColor: 'rgb('+r+','+g+', '+b+')',
+                    radius: 1000,
+                    fillOpacity: 1,
+                }).addTo(map).bindPopup(
+                    'location: ' + data[key]['location'] + ', speed: ' + data[key]['speed'] + ', lane: ' + data[key]['lane']
+                );
+                markers.push(circle);
             }
-
-            let circle = L.circle([data[key]['latitude'], data[key]['longitude']], {
-                color: 'rgb('+r+','+g+', '+b+')',
-                fillColor: 'rgb('+r+','+g+', '+b+')',
-                radius: 1000,
-            }).addTo(map).bindPopup(
-                'location: ' + data[key]['location'] + ', speed: ' + data[key]['speed'] + ', lane: ' + data[key]['lane']
-
-            );
-            markers.push(circle);
-        }
-    });
-    console.log(markers);
+            old_markers.forEach((marker) => marker.remove());
+            old_markers = [];
+        }).catch((error) => {
+            console.log(error)
+        });
 };
 
 let updater = setInterval(updateMarkers, 2000);
