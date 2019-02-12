478c725c444c6606e4fd6eea2baaf3d5fa959fb2
Tristan Trouwen <tristan.trouwen@gmail.com>
2019-02-12 17:34:10 +0100

    changed webpack conf

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,14 +1,13 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: [
         "./src"
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-        
     },
     devServer: {
         compress: true,
