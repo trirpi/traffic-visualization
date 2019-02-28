5d54352ae886260c85985c58bd10b180fb0878a6
Tristan Trouwen <tristan.trouwen@gmail.com>
2019-02-28 22:25:33 +0100

    added static folder

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,9 @@ let config = require(__dirname + '/config');
 server.listen(config.port);
 
 
+app.use(express.static(__dirname + '/public'))
+
+
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
