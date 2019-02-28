42433762b4cb019ca3a6050e9cfd3786552dfd6c
Tristan Trouwen <tristan.trouwen@gmail.com>
2019-02-28 22:44:31 +0100

    Updated structure for easier installation

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -12,5 +13,11 @@ module.exports = {
     devServer: {
         compress: true,
         port: 9000
-    }
+    },
+    plugins: [
+        new CopyPlugin([
+            { from: 'src/index.html', to: 'index.html' },
+            { from: 'src/style.css', to: 'style.css' },
+        ]),
+    ],
 }
